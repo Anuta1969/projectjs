5c3a4d4a5dc7773cfc5b427e8a9feb63601b670b
Anna <example@gmail.com>
2020-03-23 20:31:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+
+const pi = 3.14;
+
+
+let persone = {
+
+    name: "Joan",
+    age: 25,
+    isMarried: false
+
+};
+// console.log(persone["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp']; // массив
+
+// console.log(arr[0]);
+
+// alert("hello Anna");
+
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = prompt("Есть ли Вам 18?", "Да")
+
+// console.log(typeof(answer));
+
+
+let incr = 10,
+    decr = 10;
+    // incr++;
+    // decr--;
+    console.log(incr++);
+    console.log(--decr);
+
+    
